5312791f6258a1fea56b4f448a87dc3b63ce8bc6
LucasAlt <lucasalbert67@gmail.com>
2019-04-27 23:15:06 +0200

    Little example with request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var request = require('request')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  request('https://api.crypto-bridge.org/api/v1/ticker/worx_BTC', function (error, response, body) {
+    if (error) throw error
+    res.render('index', { data: JSON.parse(body) });
+  });
 });
 
 module.exports = router;
